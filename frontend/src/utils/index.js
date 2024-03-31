6c5194613d0a0eaad7619889b204528b941c9e30
Your Name <you@example.com>
2024-03-31 17:35:22 +0300

    Navbar

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,39 @@
+export const navOptions = [
+  {
+    id: "home",
+    label: "Home",
+    path: "/",
+  },
+  {
+    id: "listingNews",
+    label: "News",
+    path: "/product/listing/news",
+  },
+  {
+    id: "listing",
+    label: "Adopt",
+    path: "/product/listing/adopt",
+  },
+  {
+    id: "listingDonate",
+    label: "Donate",
+    path: "/product/listing/donate",
+  },
+  {
+    id: "listingContact",
+    label: "Contact",
+    path: "/product/listing/contact",
+  },
+];
+export const adminNavOptions = [
+  {
+    id: "adminListing",
+    label: "Manage All Dogs",
+    path: "/admin-view/all-dogs",
+  },
+  {
+    id: "adminNewProduct",
+    label: "Add New Dog",
+    path: "/admin-view/add-dog",
+  },
+];
